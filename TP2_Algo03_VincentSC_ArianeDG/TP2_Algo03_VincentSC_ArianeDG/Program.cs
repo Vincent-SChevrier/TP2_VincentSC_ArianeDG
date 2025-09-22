dd9e15b657b57a5e14c69c8be232bf394199951a
Vincent-SChevrier <1668852@cstjean.qc.ca>
2025-09-22 09:32:30 -0400

    Ajout des fichiers de base
    Ajout des fichiers de Moodle et création du projet.

---
+++ TP2_Algo03_VincentSC_ArianeDG/TP2_Algo03_VincentSC_ArianeDG/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TP2_Algo03_VincentSC_ArianeDG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
